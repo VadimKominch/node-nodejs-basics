b87a2b85481df29c67392f44c6ab2c197a5266c1
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-04 11:48:44 +0300

    feat: add solution to task4

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import fs, { write } from 'fs'
+
 export const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const fileStream = fs.createReadStream('./files/fileToRead.txt');
+    fileStream.on('data',(chunk)=> {
+        process.stdout.write(chunk.toString('utf-8'))
+    }) 
 };
+
+read()
