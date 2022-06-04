d4cc8756a81c2abf10eed9fba04e6ee38e393114
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-04 11:47:10 +0300

    feat: add solution to task2

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
+import process from 'process'
+
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const tempArray = []
+
+    for (let index = 0; index < args.length; index += 2) {
+        // Do something if you want with the group
+        tempArray.push(args.slice(index, index+2));
+    }
+    
+    console.log(tempArray
+        .map(el=>`${el[0].slice(2)} is ${el[1]}`)
+        .join("; "))
 };
+
+parseArgs()
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
+import process from 'process'
+
 export const parseEnv = () => {
-    // Write your code here 
+    const prefix = "FPS_"
+    const result = Object.keys(process.env)
+    .filter(el=>el.startsWith(prefix))
+    .map(el => `${el}=${process.env[el]}`)
+    .join("; ")
+    console.log(result)
 };
+
+parseEnv()
