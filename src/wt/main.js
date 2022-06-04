a242afc3b55971dbd39968f83cfee7eec5815f3d
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-04 11:53:07 +0300

    feat: add solution to task7

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,27 @@
+import {Worker} from "worker_threads";
+import os from 'os'
+
+
+
+
 export const performCalculations = async () => {
-    // Write your code here
+    const numOfCpus = os.cpus().length
+    const promises = []
+    
+    for(let i = 0;i<numOfCpus;i++) {
+        promises.push(new Promise(function(resolve) {
+            let worker = new Worker("./worker.js",{workerData:{num:10+i}})
+            worker.on("message",res=>{ 
+                resolve({status:'resolved',data:res})
+            })
+            worker.on('error',() => {
+                resolve({status:'error',data:null})
+            })
+        }))
+    }
+    const result = await Promise.all(promises)
+    return result
 };
+
+const res = await performCalculations()
+console.log(res)
